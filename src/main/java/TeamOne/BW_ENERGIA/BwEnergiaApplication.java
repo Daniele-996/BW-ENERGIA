cdb12aba8e7ab03b622000219bd47786130d451e
Daniele-996 <danielesanza@gmail.com>
2025-08-04 11:38:45 +0200

    Initial commit

---
+++ src/main/java/TeamOne/BW_ENERGIA/BwEnergiaApplication.java
@@ -0,0 +1,13 @@
+package TeamOne.BW_ENERGIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwEnergiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TeamOne/BW_ENERGIA/BwEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package TeamOne.BW_ENERGIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
