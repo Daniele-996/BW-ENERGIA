d71d34e4a36708cce83818b1809b980a98e76118
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-08-04 15:26:56 +0200

    Modifica di alcune entità per correzione di errori

--- src/main/java/TeamOne/BW_ENERGIA/BwEnergiaApplication.java
+++ src/main/java/TeamOne/BW_ENERGIA/BwEnergiaApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BwEnergiaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BwEnergiaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BwEnergiaApplication.class, args);
+        System.out.println("Applicazione avviata con successo!");
+    }
 
 }
