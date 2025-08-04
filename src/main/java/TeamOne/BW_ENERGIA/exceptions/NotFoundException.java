e84ed266b1114e08550917213a556eccac4ab812
Daniele-996 <danielesanza@gmail.com>
2025-08-04 17:25:15 +0200

    Added FattureDTO, FatturaService and FatturaController basic methods

---
+++ src/main/java/TeamOne/BW_ENERGIA/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package TeamOne.BW_ENERGIA.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/TeamOne/BW_ENERGIA/repositories/FatturaRepository.java
+++ src/main/java/TeamOne/BW_ENERGIA/repositories/FatturaRepository.java
@@ -1,9 +1,15 @@
 package TeamOne.BW_ENERGIA.repositories;
 
+import TeamOne.BW_ENERGIA.entities.Cliente;
 import TeamOne.BW_ENERGIA.entities.Fattura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FatturaRepository extends JpaRepository<Fattura, Long> {
+    boolean existsById(Long id);
+
+    Optional<Fattura> findByCliente(Cliente cliente);
 }
