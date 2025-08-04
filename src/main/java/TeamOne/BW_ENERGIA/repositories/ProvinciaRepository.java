d69eeed389f4417d9928f780093230da270f1ede
Ren-Mor <renatomorelli@hotmail.it>
2025-08-04 15:00:28 +0200

    repositories e classi relative a Cliente

---
+++ src/main/java/TeamOne/BW_ENERGIA/repositories/ProvinciaRepository.java
@@ -0,0 +1,9 @@
+package TeamOne.BW_ENERGIA.repositories;
+
+import TeamOne.BW_ENERGIA.entities.Provincia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProvinciaRepository extends JpaRepository<Provincia, Long> {
+}
