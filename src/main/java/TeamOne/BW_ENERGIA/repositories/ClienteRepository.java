7734230489a5b91e050b1222fe075379408c7855
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-08-06 16:35:43 +0200

    Aggiunta filtri per i clienti

--- src/main/java/TeamOne/BW_ENERGIA/repositories/ClienteRepository.java
+++ src/main/java/TeamOne/BW_ENERGIA/repositories/ClienteRepository.java
@@ -1,9 +1,24 @@
 package TeamOne.BW_ENERGIA.repositories;
 
 import TeamOne.BW_ENERGIA.entities.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    // Filtra per range di fatturato
+    Page<Cliente> findByFatturatoAnnualeBetween(int min, int max, Pageable pageable);
+
+    // Filtra per data inserimento
+    Page<Cliente> findByDataInserimento(LocalDate data, Pageable pageable);
+
+    // Filtra per data ultimo contatto
+    Page<Cliente> findByDataUltimoContatto(LocalDate data, Pageable pageable);
+
+    // Filtra per parte del nome (ragione sociale)
+    Page<Cliente> findByRagioneSocialeContainingIgnoreCase(String nome, Pageable pageable);
 }
