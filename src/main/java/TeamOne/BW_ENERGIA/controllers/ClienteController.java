a3b0f69c73f456c4df87a2bfa094f33c6b0bed31
MauroTuzzolino <tuzzolinomauro.prog05@gmail.com>
2025-08-07 11:48:28 +0200

    Modifica import

--- src/main/java/TeamOne/BW_ENERGIA/controllers/ClienteController.java
+++ src/main/java/TeamOne/BW_ENERGIA/controllers/ClienteController.java
@@ -7,10 +7,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
